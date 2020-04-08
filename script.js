71d47518aac339b5a4bf2e0c35d209605482d3e2
Opejin Abdulahi <opejinabdulahi.ay@gmail.com>
2020-04-08 23:50:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/*Array of data object*/
+data = [{principal:2500,
+    time:1.8
+},
+{principal:1000,
+    time:5
+},
+{principal:3000,
+    time:1
+},
+{principal:3000,
+    time:3
+}
+]
+
+/*function of interestCalculator*/
+function interestCalculator(Principal, Time){ ({
+    if (principal >= 2500 && time>=1){
+    rate=3
+    }
+    else if (principal >= 2500 && time<3){
+    rate=4
+    }
+    else if (principal > 2500 || time>=1){
+    rate=2
+    }
+    else {
+    rate=1
+    }
+    }
+    interest = (principal * rate * time) / 100
+    interestData.push
+})
+
+let interestData = []
+
+
+
+
+console.log(interestData)
+return interestData
+
+
+interestCalculator(data); 
